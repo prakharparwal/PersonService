f4efe3b61df03c50cfc06b10f5d3fc110c04fa31
prakharparwal <prakhar.parwal@gmail.com>
2020-02-10 10:25:47 +0530

    DevOps > PersonService> Test webhook

--- src/main/java/com/prakhar/parwal/personservice/PersonServiceApplication.java
+++ src/main/java/com/prakhar/parwal/personservice/PersonServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.prakhar.parwal.controller, com.prakhar.parwal.web.controller")
+@ComponentScan("com.prakhar.parwal.controller, com.prakhar.parwal.web.controller, com.prakhar.parwal.service")
 public class PersonServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/prakhar/parwal/service/PersonService.java
@@ -0,0 +1,8 @@
+package com.prakhar.parwal.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+}
